f5ab83dccc3ea22622713d31b5b9248d8c0d960b
learner <learnonab@hotmail.com>
2025-01-30 01:03:56 +0300

    upload project files

---
+++ README.md
@@ -0,0 +1,58 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](images/screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+For this challenges, i learned how to extract data from inputs and check for validation of the data. for example, a user cannot select number of people less than or equal to 0.
+also, i learned how to show an input field triggered with user click to a specific button that's was a new idea for me.
+finally, i learned how to show data whenever the user finish all the input fields and select a tip amount and clear the data if the user click on "clear" button.
+
+### Continued development
+
+I would focus more on trying to come up with new ideas that's help me for learning new things.
+
+## Author
+
+- Frontend Mentor - [@abdulrrahmann](https://www.frontendmentor.io/profile/abdulrrahmann)
---
+++ main.js
@@ -0,0 +1,190 @@
+// Define variables
+let bill = document.getElementById("bill");
+let billNum;
+let tipRatio;
+let tipAmount;
+let tipTotal;
+let numberOfPeople = document.getElementById("number");
+let numberOfPeopleNum;
+let tip = document.getElementById("tip");
+let tipButtons = document.querySelectorAll("ul.select-tip li");
+let myForm = document.querySelector("form");
+let resetBtn = document.querySelector(".result .btn");
+// handle bill
+bill.addEventListener("input", (e) => {
+  if (Number(e.target.value) <= 0) {
+    billNum = 0;
+    myForm.querySelectorAll("div.input-container")[0].classList.add("error");
+    myForm.querySelectorAll("label.info .msg")[0].classList.remove("hidden");
+
+    // Set the spans to 0's
+    document.querySelector(".tip-amount .right span").innerHTML = "0.00";
+    document.querySelector(".total .right span").innerHTML = "0.00";
+  } else {
+    myForm.querySelectorAll("div.input-container")[0].classList.remove("error");
+    myForm.querySelectorAll("label.info .msg")[0].classList.add("hidden");
+    billNum = Number(e.target.value);
+
+    // Calc tipAmount and Total
+    if (numberOfPeopleNum && tipRatio && billNum) {
+      tipAmount = (billNum * tipRatio).toFixed(2);
+      tipTotal = (numberOfPeopleNum * tipAmount).toFixed(2);
+
+      // Update tip Amount and total
+      document.querySelector(".tip-amount .right span").innerHTML = tipAmount;
+      document.querySelector(".total .right span").innerHTML = tipTotal;
+
+      // add active to reset Btn
+      resetBtn.classList.add("active");
+    } else {
+      // Set the spans to 0's
+      document.querySelector(".tip-amount .right span").innerHTML = "0.00";
+      document.querySelector(".total .right span").innerHTML = "0.00";
+    }
+  }
+});
+
+// handle NumberOfPeople
+numberOfPeople.addEventListener("input", (e) => {
+  if (Number(e.target.value) <= 0) {
+    numberOfPeopleNum = 0;
+    myForm.querySelectorAll("div.input-container")[1].classList.add("error");
+    myForm.querySelectorAll("label.info .msg")[1].classList.remove("hidden");
+
+    // Set the spans to 0's
+    document.querySelector(".tip-amount .right span").innerHTML = "0.00";
+    document.querySelector(".total .right span").innerHTML = "0.00";
+  } else {
+    myForm.querySelectorAll("div.input-container")[1].classList.remove("error");
+    myForm.querySelectorAll("label.info .msg")[1].classList.add("hidden");
+    numberOfPeopleNum = Number(e.target.value);
+
+    // Calc tipAmount and Total
+    if (billNum && tipRatio && numberOfPeopleNum) {
+      tipAmount = (billNum * tipRatio).toFixed(2);
+      tipTotal = (numberOfPeopleNum * tipAmount).toFixed(2);
+
+      // Update tip Amount and total
+      document.querySelector(".tip-amount .right span").innerHTML = tipAmount;
+      document.querySelector(".total .right span").innerHTML = tipTotal;
+
+      // add active to reset Btn
+      resetBtn.classList.add("active");
+    } else {
+      // Set the spans to 0's
+      document.querySelector(".tip-amount .right span").innerHTML = "0.00";
+      document.querySelector(".total .right span").innerHTML = "0.00";
+    }
+  }
+});
+
+// handle tips
+tipButtons.forEach((button) => {
+  // check if it's custom
+
+  button.addEventListener("click", (e) => {
+    if (
+      e.target.parentElement.id === "custom-btn" ||
+      e.target.id === "custom-btn"
+    ) {
+      // Remove active class from all lis
+      tipButtons.forEach((button) => {
+        button.classList.remove("active");
+      });
+      document.querySelector("#custom-btn span").classList.add("hidden");
+      tip.classList.remove("hidden");
+      document.querySelector("#custom-btn").classList.add("custom");
+
+      tip.addEventListener("input", (e) => {
+        if (Number(e.target.value) >= 0) {
+          if (Number(e.target.value) > 100) {
+            e.target.value = 100;
+          }
+          tipRatio = Number(e.target.value) / 100;
+
+          // Calc tipAmount and Total
+          if (billNum && numberOfPeopleNum && tipRatio) {
+            tipAmount = (billNum * tipRatio).toFixed(2);
+            tipTotal = (numberOfPeopleNum * tipAmount).toFixed(2);
+
+            // Update tip Amount and total
+            document.querySelector(".tip-amount .right span").innerHTML =
+              tipAmount;
+            document.querySelector(".total .right span").innerHTML = tipTotal;
+
+            // add active to reset Btn
+            resetBtn.classList.add("active");
+          } else {
+            // Set the spans to 0's
+            document.querySelector(".tip-amount .right span").innerHTML =
+              "0.00";
+            document.querySelector(".total .right span").innerHTML = "0.00";
+          }
+        } else {
+          tip.value = 0;
+        }
+      });
+    } else {
+      document.querySelector("#custom-btn span").classList.remove("hidden");
+      tip.value = 0;
+      tip.classList.add("hidden");
+      document.getElementById("custom-btn").classList.remove("custom");
+
+      tipRatio = Number(button.dataset.amount) / 100;
+
+      tipButtons.forEach((button) => {
+        button.classList.remove("active");
+      });
+
+      e.target.classList.add("active");
+
+      // Calc tipAmount and Total
+      if (billNum && numberOfPeopleNum && tipRatio) {
+        tipAmount = (billNum * tipRatio).toFixed(2);
+        tipTotal = (numberOfPeopleNum * tipAmount).toFixed(2);
+
+        // Update tip Amount and total
+        document.querySelector(".tip-amount .right span").innerHTML = tipAmount;
+        document.querySelector(".total .right span").innerHTML = tipTotal;
+
+        // add active to reset Btn
+        resetBtn.classList.add("active");
+      } else {
+        // Set the spans to 0's
+        document.querySelector(".tip-amount .right span").innerHTML = "0.00";
+        document.querySelector(".total .right span").innerHTML = "0.00";
+      }
+    }
+  });
+});
+
+// Reset Button
+resetBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // Clear the form
+  myForm.reset();
+  // Clear tip ratio, amount and number
+  tipRatio = 0;
+  billNum = 0;
+  numberOfPeopleNum = 0;
+  // Set the spans to 0's
+  document.querySelector(".tip-amount .right span").innerHTML = "0.00";
+  document.querySelector(".total .right span").innerHTML = "0.00";
+
+  // Remove active class from tip li
+  tipButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  // Remove active class from the button
+    e.target.classList.remove("active");
+    
+    // hide custom button
+    if (document.getElementById("custom-btn").classList.contains("custom")) {
+        document.querySelector("#custom-btn span").classList.remove("hidden");
+        tip.value = 0;
+        tip.classList.add("hidden");
+        document.getElementById("custom-btn").classList.remove("custom");
+    }
+});
